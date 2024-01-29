2532ff4d507023e0e164b9e960814917dfbab175
Yaki <fedemontanari@outlook.com>
2024-01-29 20:44:34 -0300

    Added contact hrefs display

--- src/app/contacto/page.tsx
+++ src/app/contacto/page.tsx
@@ -1,17 +1,29 @@
 import { Button } from "@/components/ui/button";
+import { prisma } from "@/lib/prisma";
 import Link from "next/link";
 import React from "react";
 
-export default function Contact() {
+const fetchData = async () => {
+  const content = await prisma.contact.findMany();
+  return content;
+};
+
+export default async function Contact() {
+  const content = await fetchData();
   return (
     <div className="flex flex-col items-center justify-center gap-5">
       <h1 className="font-extrabold text-4xl">Contacto</h1>
-      <Button asChild>
-        <Link href="#">Instagram</Link>
-      </Button>
-      <Button asChild>
-        <Link href="#">Whatsapp</Link>
-      </Button>
+      {content.length ? (
+        content.map((el) => {
+          return (
+            <Button asChild key={el.id}>
+              <Link href={el.url}>{el.name}</Link>
+            </Button>
+          );
+        })
+      ) : (
+        <p>Problemas cargando el contendio, por favor intente de nuevo.</p>
+      )}
     </div>
   );
 }
