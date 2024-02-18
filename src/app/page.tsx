6fa6bd29c7f5563fa98f8c90d3d6897bfe32f807
Yaki <fedemontanari@outlook.com>
2024-02-18 14:10:32 -0300

    Modified landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@
 import fetchPictures from "@/utils/fetchPictures";
 import { useEffect, useState } from "react";
 import Image from "next/image";
+import pfp from "@/assets/images/florpic.jpg";
+import Link from "next/link";
 
 export default function Home() {
   const [imgUrls, setImgUrls] = useState<string[]>([]);
@@ -21,25 +23,31 @@ export default function Home() {
           <span className="font-arsenica">fotogaleria</span>
         </h2>
       </div>
-      <div>
-        <ul className="flex flex-row flex-wrap justify-center gap-3">
-          {imgUrls.map((url, i) => {
-            return (
-              <li
-                key={i}
-                className="relative h-[300px] transition-transform duration-200 hover:z-50 hover:scale-110 hover:shadow-lg"
-              >
-                <Image
-                  src={url}
-                  alt="Image"
-                  width={360}
-                  height={1}
-                  className="h-full w-full rounded-md object-cover align-middle"
-                />
-              </li>
-            );
-          })}
-        </ul>
+      <div className="grid grid-cols-2 items-center gap-3 px-4">
+        <Image src={pfp} alt="Foto de Perfil" className="rounded-lg" />
+        <p className="text-center text-lg italic text-white">
+          ¡Bienvenidos! Gracias por pasar y tomarse el tiempo de venir a mí
+          lugar en el mundo. Los invito a conocerme mejor en la sección llamada{" "}
+          <Link href="/sobremi" className="text-second">
+            Sobre Mí
+          </Link>
+          . En ese espacio les cuento lo que significa para mí el arte de la
+          fotografía. <br />
+          En la sección{" "}
+          <Link href="/albums" className="text-second">
+            Albums
+          </Link>{" "}
+          podrán observar las distintas áreas a las que me dedico. Cada una de
+          ellas está realizada con mucha creatividad y mucho amor para todos
+          ustedes. <br /> Y por último en la sección{" "}
+          <Link href="/contacto" className="text-second">
+            Contacto
+          </Link> podrán
+          comunicarse directamente conmigo a través de mis redes, lo que nos
+          permitirá acercarnos para crear momentos increíbles a través de la
+          fotografía. <br /> Espero que les guste. <br /> ¡Muchas gracias y
+          bonito día! <br /> <b>Florencia Elías 📸</b>
+        </p>
       </div>
     </main>
   );
